dcc1fe49771ab63c356835c689c54e20dfa27632
Dmitrii Morozov <33morozov@gmail.com>
2024-05-08 14:28:07 +0200

    fix: bag in win conditional

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from constants import NUM_ROW, NUM_COL, EMPTY_CELL, WIN_NUM
 from utils import WinGame, InvalidColumn, FullBoard
 
@@ -10,9 +12,10 @@ class Board:
         self.empty_cell = EMPTY_CELL
         self.win_num = WIN_NUM
         self.board = self._get_empty_board()
-        self.delta = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+        self.delta = [(0, 1), (0, -1), (1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
 
-    def _get_empty_board(self):
+    def _get_empty_board(self) -> List[List[int]]:
+        # TODO Add sentinel element, like -1
         return [[self.empty_cell for _ in range(self.col)] for _ in range(self.row)]
 
     def show_board(self):
@@ -20,7 +23,7 @@ class Board:
             print(" ".join(self.board[i]))
         print(" ".join([str(x) for x in range(self.col)]))
 
-    def put_chip(self, col: int, player: str):
+    def put_chip(self, col: int, player: str) -> None:
         if col < 0 or col >= self.col:
             raise InvalidColumn("Invalid column")
 
@@ -35,20 +38,21 @@ class Board:
     def check_win_cell(self, row: int, col: int, player: str) -> None:
         """Check win conditional for all directions"""
         for delta_row, delta_col in self.delta:
+            # print(row, col, delta_row, delta_col, player)
             if self._check_win(row, col, delta_row, delta_col, player):
                 raise WinGame(f"Player {player} is win!")
 
-    def _check_win(self, row: int, col: int, delta_row: int, delta_col: int, player:str):
-        for _ in range(self.win_num -1):
+    def _check_win(self, row: int, col: int, delta_row: int, delta_col: int, player: str) -> bool:
+        for _ in range(self.win_num - 1):
             row += delta_row
             col += delta_col
             if (
-                    row < 0
-                    or row >= self.row - 1
-                    or col < 0
-                    or col >= self.col - 1
-                    or self.board[col][row] != player
+                0 <= row < self.row
+                and 0 <= col < self.col
+                and self.board[row][col] == player
             ):
+                continue
+            else:
                 return False
         return True
 
